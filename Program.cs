ec0ea7ded6ae0b20569ed65b4dcd5829f6862e2a
Diele <dybs1520@gmail.com>
2023-12-26 23:36:40 -0300

    Criação Main

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Treinando_logica.Models;
+
+internal class Programa
+{
+    public static void Main(string[] args)
+    {
+        Exercicio1 exercicio = new Exercicio1();
+        exercicio.calcular();
+    }
+}
+
+
+
+
